cd5312502acd65992660255fd6d1eca81af7618e
navyakoppana <121319053+navyakoppana@users.noreply.github.com>
2024-06-11 13:41:32 -0500

    Create Soccer_analysis.sql

---
+++ Soccer_analysis.sql
@@ -0,0 +1,13 @@
+#Extraction of matches.csv file from sqlite file using Pandas
+#Note:All other csv files are extracted from other tables in same manner.
+import pandas as pd
+import sqlite3 
+conn = sqlite3.connect('database.sqlite')
+c = conn.cursor()
+c.execute("""SELECT * FROM Match;""")
+match = pd.DataFrame(c.fetchall())
+match.columns = [x[0] for x in c.description]
+match=match.iloc[:,:11]
+match.to_csv('matches.csv',index=False)
+**-------------------------------------------------------------------------------------------------------------------------------------**
+**-------------------------------------------------------------------------------------------------------------------------------------**
