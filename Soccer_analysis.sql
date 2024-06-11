bff061b92cea69cf83b4aea2a3214b632a1c33d5
navyakoppana <121319053+navyakoppana@users.noreply.github.com>
2024-06-11 13:43:38 -0500

    Update Soccer_analysis.sql

--- Soccer_analysis.sql
+++ Soccer_analysis.sql
@@ -1,4 +1,4 @@
-#Extraction of matches.csv file from sqlite file using Pandas
+**Extraction of matches.csv file from sqlite file using Pandas**
 #Note:All other csv files are extracted from other tables in same manner.
 import pandas as pd
 import sqlite3 
@@ -9,5 +9,5 @@ match = pd.DataFrame(c.fetchall())
 match.columns = [x[0] for x in c.description]
 match=match.iloc[:,:11]
 match.to_csv('matches.csv',index=False)
-**-------------------------------------------------------------------------------------------------------------------------------------**
-**-------------------------------------------------------------------------------------------------------------------------------------**
+*********************************************************************************************
+*********************************************************************************************
